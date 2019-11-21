677292d5de662e4bdc7d18ca8ff173e04ec85959
dillonryan98 <dillonryan98@gmail.com>
2019-11-21 10:38:03 +0000

    ^ relational tables created

--- cyclingdb.sql
+++ cyclingdb.sql
@@ -15,8 +15,6 @@ INSERT INTO `Members` (`Member_ID`, `Member_Name`, `Email`, `Speciality`, `Phone
 (`7153969`, `Conor Jupe`, `ConorJupe@cycyling.com`,`Climber`, `0874788764`),
 (`7120685`, `Liam Manning`, `LiamManning@cycyling.com`,`Puncher`, `08705867594`);
 
-
-
 CREATE TABLE IF NOT EXISTS `Coach` (
   `Coaches_ID` int(6) NOT NULL,
   `Coaches_Name` varchar(30) NOT NULL,
@@ -32,10 +30,6 @@ INSERT INTO `Coach` (`Coaches_ID`, `Coaches_Name`, `Email`, `Position`, `Phone_N
 (`178654`, `Stephen Roche`, `StephenRoche@cycylingcoaching.com`,`Sprint Coach`, `0871236549`),
 (`179874`, `Sean Kinsella`, `SeanKinsella@cycylingcoaching.com`,`Endurance Coach`, `0879574263`),
 (`171233`, `Patrick Fenning`, `PatrickFenning@cycylingcoaching.com`,`Hill Coach`, `0871947556`);
-
-
-
-
 
 CREATE TABLE IF NOT EXISTS `Races` (
   `Race_ID` int(6) NOT NULL,
@@ -53,9 +47,6 @@ CREATE TABLE IF NOT EXISTS `Races` (
  (`34565`, `Combe_Connors`, `130`, `Flat`, `DublinCC`),
  (`66544`, `SheepsLeg`, `80`, `Mountainous`, `WestRoadCC`);
  
- 
- 
- 
  CREATE TABLE IF NOT EXISTS `Training` (
   `Date` date NOT NULL,
   `Location` varchar(30) NOT NULL,
@@ -71,6 +62,42 @@ CREATE TABLE IF NOT EXISTS `Races` (
  (`01/05/2019`, `Dublin`, `80`, `Mountainous`, `Caolan_Foley`),
  (`08/05/2019`, `Dublin`, `105`, `Hilly`, `Caolan_Foley`),
  (`16/05/2019`, `Kildare`, `140`, `Flat`, `John_Smith`);
+ 
+  CREATE TABLE IF NOT EXISTS `Equipment` (
+  `Name` varchar(50) NOT NULL,
+  `OnLoan` boolean NOT NULL,
+  `Value` int(7) NOT NULL,
+  `How_Old_Years` int(2) NOT NULL,
+  `Equipment_ID` int(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+ INSERT INTO `Equipment` (`Name`, `onLoan`, `Value`, `How_Old`, `Equipment_ID`) VALUES
+ (`Race_Wheels`, `True`, `800`, `1`, `432343`),
+ (`Lightweight_Wheels`, `True`, `500`, `3`, `433443`),
+ (`Turbo_Trainer`, `False`, `250`, `0`, `128593`),
+ (`Skinsuit`, `False`, `420`, `4`, `448258`),
+ (`Race_Shoes`, `False`, `300`, `2`, `456534`),
+ (`Jersey`, `True`, `50`, `0`, `4`, `468234`),
+ (`Tyres`, `False`, `130`, `2`, `439902`);
+ 
+   CREATE TABLE IF NOT EXISTS `Registration` (
+  `Name` varchar(50) NOT NULL,
+  `Previous_Member` boolean NOT NULL,
+  `Registration_Fee` int(7) NOT NULL,
+  `Members_Age` int(4) NOT NULL,
+  `Members_ID` int(7) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+INSERT INTO `Registration` (`Name`, `Previous_Member`, `Reistration_Fee`, `Members_Age`, `Members_ID`) VALUES 
+ (`Sean_Micheals`, `True`, `80`, `22`, `7192857`), 
+ (`AJ_Tracey`, `True`, `120`, `34`, `7167445`), 
+ (`Chris_Jericho`, `False`, `120`, `28`, `7101938`), 
+ (`Randy_Orton`, `True`, `30`, `14`, `7119385`), 
+ (`David_Finlay`, `False`, `120`, `42`, `7101834`), 
+ (`Jeff_Hardy`, `False`, `80`, `19`, `7138647`); 
+
+
  
  
  
