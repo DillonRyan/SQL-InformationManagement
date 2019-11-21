dbb834e0f8ca0a94da1937c3e8b0e69ef0568dd5
dillonryan98 <dillonryan98@gmail.com>
2019-11-21 18:18:56 +0000

    Finished writing the code and started report

--- cyclingdb.sql
+++ cyclingdb.sql
@@ -34,12 +34,12 @@ INSERT INTO `Coach` (`Coaches_ID`, `Coaches_Name`, `Email`, `Position`, `Phone_N
 CREATE TABLE IF NOT EXISTS `Races` (
   `Race_ID` int(6) NOT NULL,
   `Race_Name` varchar(30) NOT NULL,
-  `Distance_Km` int(50) NOT NULL,
+  `Distance_KM` int(50) NOT NULL,
   `Terrain` varchar(50) NOT NULL,
   `Race_Organisers` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
- INSERT INTO `Races` (`Race_ID`, `Ra_Name`, `Distance_Km`, `Terrain`, `Race_Organisers`) VALUES
+ INSERT INTO `Races` (`Race_ID`, `Ra_Name`, `Distance_KM`, `Terrain`, `Race_Organisers`) VALUES
  (`87654`, `EastSideRR`, `120`, `Hilly`, `EastSideCC`),
  (`34553`, `OverTheHill`, `145`, `Hilly`, `KidareCC`),
  (`34654`, `FlatOut`, `180`, `Flat`, `LeinsterCC`),
@@ -52,16 +52,16 @@ CREATE TABLE IF NOT EXISTS `Races` (
   `Location` varchar(30) NOT NULL,
   `Training_Distance` int(50) NOT NULL,
   `Terrain` varchar(50) NOT NULL,
-  `Coaches_Attending` varchar(30) NOT NULL
+  `Coaches_ID` int(6) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
- INSERT INTO `Training` (`Date`, `Location`, `Training_Distance`, `Terrain`, `Coaches_Attending`) VALUES
- (`11/04/2019`, `Kildare`, `90`, `Hilly`, `Patrick_Fenning`),
- (`18/04/2019`, `Dublin`, `130`, `Flat`, `John_Cena`),
- (`25/04/2019`, `Kildare`, `110`, `Rolly`, `Patrick_Fenning`),
- (`01/05/2019`, `Dublin`, `80`, `Mountainous`, `Caolan_Foley`),
- (`08/05/2019`, `Dublin`, `105`, `Hilly`, `Caolan_Foley`),
- (`16/05/2019`, `Kildare`, `140`, `Flat`, `John_Smith`);
+ INSERT INTO `Training` (`Date`, `Location`, `Training_Distance`, `Terrain`, `Coaches_ID`) VALUES
+ (`11/04/2019`, `Kildare`, `90`, `Hilly`, `17383764`),
+ (`18/04/2019`, `Dublin`, `130`, `Flat`, `172948`),
+ (`25/04/2019`, `Kildare`, `110`, `Rolly`, `173980`),
+ (`01/05/2019`, `Dublin`, `80`, `Mountainous`, `170387`),
+ (`08/05/2019`, `Dublin`, `105`, `Hilly`, `172009`),
+ (`16/05/2019`, `Kildare`, `140`, `Flat`, `172388`);
  
   CREATE TABLE IF NOT EXISTS `Equipment` (
   `Name` varchar(50) NOT NULL,
@@ -81,7 +81,7 @@ CREATE TABLE IF NOT EXISTS `Races` (
  (`Tyres`, `False`, `130`, `2`, `439902`);
  
    CREATE TABLE IF NOT EXISTS `Registration` (
-  `Name` varchar(50) NOT NULL,
+  `Member_Name` varchar(50) NOT NULL,
   `Previous_Member` boolean NOT NULL,
   `Registration_Fee` int(7) NOT NULL,
   `Members_Age` int(4) NOT NULL,
@@ -89,7 +89,7 @@ CREATE TABLE IF NOT EXISTS `Races` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
-INSERT INTO `Registration` (`Name`, `Previous_Member`, `Reistration_Fee`, `Members_Age`, `Members_ID`) VALUES 
+INSERT INTO `Registration` (`Member_Name`, `Previous_Member`, `Reistration_Fee`, `Members_Age`, `Members_ID`) VALUES 
  (`Sean_Micheals`, `True`, `80`, `22`, `7192857`), 
  (`AJ_Tracey`, `True`, `120`, `34`, `7167445`), 
  (`Chris_Jericho`, `False`, `120`, `28`, `7101938`), 
@@ -100,9 +100,44 @@ INSERT INTO `Registration` (`Name`, `Previous_Member`, `Reistration_Fee`, `Membe
 
  
  
+ ALTER TABLE `Coach`
+ ADD primary key (`Coaches_ID`);
  
  
+ ALTER TABLE `Members`
+ ADD primary key (`Member_ID`);
  
+ 
+ ALTER TABLE `Races`
+ ADD primary key (`Race_ID`);
+ 
+ 
+ ALTER TABLE `Training`
+ ADD primary key (`Coaches_Id`);
+ 
+ 
+ ALTER TABLE `Equipment`
+ ADD primary key (`Equipment_ID`);
+ 
+ 
+ ALTER TABLE `Registration`
+	ADD primary key (`Member_ID`);
+ 
+ 
+ ALTER TABLE `Members`
+ ADD CONSTRAINT `Members_Con` foreign key (`Members_ID`) references `Registrations` (`Members_ID`),
+ ADD CONSTRAINT `Members_Con1` foreign key (`Members_Name`) references `Registrations` (`Members_Name`);
+ 
+ ALTER TABLE `Coach`
+ ADD CONSTRAINT `Coach_Con` foreign key (`Coches_ID`) references `Training` (`Coches_ID`);
+ 
+ 
+ CREATE VIEW Equip_On_Loan AS 
+ SELECT `Name`
+ FROM `Equipment`
+ WHERE `On_Loan` = true;
+ 
+
  
  
  
